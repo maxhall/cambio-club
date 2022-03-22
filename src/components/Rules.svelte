c07a3d98deba9e00b5122fdbd5c0ee24f0bf96a8
Max Hall <maxwelljhall@gmail.com>
2022-03-22 19:05:46 +1100

    Add card illustrations to rules

--- src/components/Rules.svelte
+++ src/components/Rules.svelte
@@ -1,44 +1,176 @@
 <h2>How to play</h2>
-<p>Cambio is a game for two or more people, played with a standard deck and two jokers. It's a memory and card swapping game, quite similar to Cabo.</p>
-<p>The winner is the player with the lowest score when each player adds their card values together at the end of the game. The card values are:</p>
+<p>
+  Cambio is a game for two or more people, played with a standard deck and two
+  jokers. It's a memory and card swapping game, quite similar to Cabo.
+</p>
+<p>
+  The winner is the player with the lowest score when each player adds their
+  card values together at the end of the game. The card values are:
+</p>
+<div class="wrapper">
+  <ul>
+    <li>
+      <img
+        src="/illustrations/black-king.svg"
+        alt="A black king playing card"
+      /> -1 for a Black king
+    </li>
+    <li>
+      <img src="/illustrations/joker.svg" alt="A joker playing card" /> 0 for a Joker
+    </li>
+    <li>
+      <img src="/illustrations/ace.svg" alt="An ace playing card" /> 1 for any Ace
+    </li>
+    <li>
+      <img src="/illustrations/jack.svg" alt="A jack playing card" />
+      <img src="/illustrations/queen.svg" alt="A queen playing card" /> 10 for any
+      Jack or Queen
+    </li>
+    <li>
+      <img src="/illustrations/red-king.svg" alt="A red king playing card" /> 40
+      for a Red King
+    </li>
+    <li>
+      <img src="/illustrations/2.svg" alt="A number 2 playing card" /><img
+        src="/illustrations/7.svg"
+        alt="A number 7 playing card"
+      /> Other numbered cards like 2 or 7 are worth their face value
+    </li>
+  </ul>
+</div>
+<p>
+  Each player starts with four cards, two rows of two cards, in front of them.
+  The remaining deck is placed in the centre and one is drawn face up next to it
+  to start a pile. Before starting, each player may peek once at their two
+  closest cards.
+</p>
+<p>
+  Players then take turns, going clockwise from the player to the dealer's left.
+  Each turn, the player may pass or choose one of three actions:
+</p>
 <ul>
-    <li>-1 — Black king</li>
-    <li>0 — Joker</li>
-    <li>1 — Ace</li>
-    <li>10 — Jack or Queen</li>
-    <li>40 — Red King</li>
-    <li>Numbers are worth their face value</li>
-</ul>
-<p>Each player starts with four cards, two rows of two cards, in front of them. The remaining deck is placed in the centre and one is drawn face up next to it to start a pile. Before starting, each player may peek once at their two closest cards.</p>
-<p>Players then take turns, going clockwise from the player to the dealer's left. Each turn, the player may pass or choose one of three actions:</p>
-<ul>
-    <li>Pick up the top pile card and swap it with one of their own</li>
-    <li>Pick up the top deck card and decide whether to place the card face up on the pile or swap it with one of their cards. If the latter, the swapped card is placed face up on the pile</li>
-    <li>Declare "Cambio" to end their turn and begin the end of the game. After each other player has taken a final turn, all cards are turned face up and scores are tallied</li>
+  <li>Pick up the top pile card and swap it with one of their own</li>
+  <li>
+    Pick up the top deck card and decide whether to place the card face up on
+    the pile or swap it with one of their cards. If the latter, the swapped card
+    is placed face up on the pile
+  </li>
+  <li>
+    Declare "Cambio" to end their turn and begin the end of the game. After each
+    other player has taken a final turn, all cards are turned face up and scores
+    are tallied
+  </li>
 </ul>
 <h3>Special cards</h3>
-<p>Some cards grant a special action when a player places them on the pile during their turn:</p>
+<p>
+  Some cards grant a special action when a player places them on the pile during
+  their turn:
+</p>
 <ul>
-    <li>Seven or eight — Optionally look at a card belonging to another player</li>
-    <li>Nine or ten — Optionally look at one of your own cards</li>
-    <li>Jack — Exchange one of your cards for another player's without looking at either card</li>
-    <li>Queen — Optionally look at another player's card, then decide whether to swap another players' card for one of your own. The card you look at does not need to be the one you swap.</li>
+  <li>
+    Seven or eight — Optionally look at a card belonging to another player
+  </li>
+  <li>Nine or ten — Optionally look at one of your own cards</li>
+  <li>
+    Jack — Exchange one of your cards for another player's without looking at
+    either card
+  </li>
+  <li>
+    Queen — Optionally look at another player's card, then decide whether to
+    swap another players' card for one of your own. The card you look at does
+    not need to be the one you swap.
+  </li>
 </ul>
 <h3>Snapping</h3>
-<p>The fewer cards a player has the better their chance of winning. To get rid of cards, a player can snap a card onto the pile if its rank matches the visible card. For example, if the visible pile card is a two of hearts, a player with a two of any suit could immediately place it on the pile.</p>
-<p>This can happen any time, including during another player's turn or while peeking at the closest two cards at the game's start, but each card can only be snapped onto once.</p>
-<p>A player who snaps incorrectly draws a penalty card from the deck and places it in front of them without looking at it. If a card with an associated action is snapped, the action is not triggered a second time. Play returns to the player who originally played the card and they take their action.</p>
-<p>Tip: Tapping the "s" key on your keyboard is the same a tapping the "snap" button.</p>
+<p>
+  The fewer cards a player has the better their chance of winning. To get rid of
+  cards, a player can snap a card onto the pile if its rank matches the visible
+  card. For example, if the visible pile card is a two of hearts, a player with
+  a two of any suit could immediately place it on the pile.
+</p>
+<p>
+  This can happen any time, including during another player's turn or while
+  peeking at the closest two cards at the game's start, but each card can only
+  be snapped onto once.
+</p>
+<p>
+  A player who snaps incorrectly draws a penalty card from the deck and places
+  it in front of them without looking at it. If a card with an associated action
+  is snapped, the action is not triggered a second time. Play returns to the
+  player who originally played the card and they take their action.
+</p>
+<p>
+  Tip: Tapping the "s" key on your keyboard is the same a tapping the "snap"
+  button.
+</p>
 <h3>Options</h3>
 <ul>
-    <li>"Allow snapping others' cards" lets you snap another player's card. It's the default on this site, because we're not cowards. If a player snaps correctly they can place one of their cards where the snapped card was. But if they get it wrong, they have to take the player's card and a penalty card.</li>
-    <li>"Risky fives" changes the way points are counted at the game's end. Instead of counting five cards normally, players with two five cards subtract 25 points from their total and those with only one five card add 25 points.</li>
-    <li>"Open hands" shows everyone's cards face up at all times, which can be useful when learning to play.</li>
+  <li>
+    "Allow snapping others' cards" lets you snap another player's card. It's the
+    default on this site, because we're not cowards. If a player snaps correctly
+    they can place one of their cards where the snapped card was. But if they
+    get it wrong, they have to take the player's card and a penalty card.
+  </li>
+  <li>
+    "Risky fives" changes the way points are counted at the game's end. Instead
+    of counting five cards normally, players with two five cards subtract 25
+    points from their total and those with only one five card add 25 points.
+  </li>
+  <li>
+    "Open hands" shows everyone's cards face up at all times, which can be
+    useful when learning to play.
+  </li>
 </ul>
 <h3>Playing offline</h3>
 <p>A few pointers for playing in the flesh:</p>
 <ul>
-    <li>When multiple players snap at the same time, the first to place their matching card on the pile succeeds and any other player that lifted a card off the table to snap takes a penalty</li>
-    <li>Players should not rearrange their cards during the game. It's easiest to think of each card as maintaining its position in the grid of four cards at the start of the game. When swapping between players you swap between fixed position. When drawing a penalty card, fill an empty position if one is available, otherwise place the penalty card to one side of your cards</li>
-    <li>Replenish an empty deck by shuffling all but the top card of the pile.</li>
+  <li>
+    When multiple players snap at the same time, the first to place their
+    matching card on the pile succeeds and any other player that lifted a card
+    off the table to snap takes a penalty
+  </li>
+  <li>
+    Players should not rearrange their cards during the game. It's easiest to
+    think of each card as maintaining its position in the grid of four cards at
+    the start of the game. When swapping between players you swap between fixed
+    position. When drawing a penalty card, fill an empty position if one is
+    available, otherwise place the penalty card to one side of your cards
+  </li>
+  <li>
+    Replenish an empty deck by shuffling all but the top card of the pile.
+  </li>
 </ul>
+
+<style>
+  li {
+    margin-bottom: 0.5rem;
+  }
+  
+  .wrapper {
+    background: var(--game-bg-lightest);
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    margin: 0 0 1rem 0;
+  }
+
+  .wrapper ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  .wrapper li {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin: 0;
+  }
+
+  .wrapper img {
+    width: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  .wrapper li + li {
+    margin-top: 0.5rem;
+  }
+</style>
